301aa11a9cfb3cc949e7b17cc7298f4d7078c658
Bjarne Wittlieb <bjarne.wittlieb@adesso.de>
2025-01-24 21:26:46 +0100

    Added basic bubble script

---
+++ Assets/BasicBubble.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// Basic Bubble has no special effects. It is selectable.
+/// </summary>
+public class BasicBubble : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
