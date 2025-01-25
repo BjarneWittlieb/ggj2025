4f6133d7e1cdb60106cff62164df0b7c5f736be2
muhsfeldt <carsten.muhsfeldt@adesso.de>
2025-01-26 00:26:13 +0100

    add sprite replacing and particle effect to popping

--- Assets/BubbleBase.cs
+++ Assets/BubbleBase.cs
@@ -7,6 +7,8 @@ public class BubbleBase : MonoBehaviour
     [SerializeField]  public string     name;
     [HideInInspector] public Vector2Int gridPosition;
     
+    public GameObject particleEffectPrefab;
+    
     private bool       isPopped;
     private ScoreLogic scoreLogic;
 
@@ -14,16 +16,26 @@ public class BubbleBase : MonoBehaviour
     protected virtual void Start()
     {
         scoreLogic = GameObject.Find("ScoreLogic").GetComponent<ScoreLogic>();
+        particleEffectPrefab = Resources.Load<GameObject>("Prefabs/Airpop");
     }
 
     public virtual void Pop()
     {
-        if (!isPopped)
-            isPopped = true;
-
+        if (isPopped)
+            return;
+        
+        isPopped = true;
+        
+        // ParticleEffect
+        GameObject effect = Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
+        Destroy(effect, 2f);
+        
+        // Score
         scoreLogic.AddToScore(score);
 
-        // TODO start pop animation and sound and such
-        Destroy(gameObject);
+        // replace sprite with random popped-sprite
+        SpriteRenderer renderer = (SpriteRenderer)gameObject.GetComponentInChildren(typeof(SpriteRenderer));
+        int randomNumber = Random.Range(1, 4);
+        renderer.sprite = Resources.Load<Sprite>("Sprites/bubbles-popped" + randomNumber);
     }
 }
