fc009612e0d829facc848f81d0abb1a36e2baee7
Armbrust, Cornelius <Cornelius.Armbrust@adesso.de>
2025-01-25 13:08:03 +0100

    kkkk

---
+++ Assets/BubbleWobble.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BubbleWobble : MonoBehaviour
+{
+    public  float wobbleSpeed = 2f;   
+    public  float wobbleScale = 0.05f; 
+
+    private Vector3 initialScale;
+    private float   randomOffsetX;       
+    private float   randomOffsetY;  
+
+    void Start()
+    {
+        initialScale  =  transform.localScale;
+        wobbleSpeed   += Random.Range(-.2f, .2f);
+        wobbleScale   += Random.Range(-.05f, .05f);
+        randomOffsetX =  Random.Range(0f, Mathf.PI * 2f);
+        randomOffsetY =  Random.Range(0f, Mathf.PI * 2f);
+    }
+
+    void Update()
+    {
+        float wobbleX = Mathf.Sin(Time.time * wobbleSpeed + randomOffsetX) * wobbleScale;
+        float wobbleY = Mathf.Cos(Time.time * wobbleSpeed + randomOffsetY) * wobbleScale;
+
+        // wobble to scale
+        transform.localScale = new Vector3(
+                                           initialScale.x + wobbleX,
+                                           initialScale.y + wobbleY,
+                                           initialScale.z
+                                          );
+
+        // rotate???
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * wobbleSpeed + randomOffsetX) * wobbleScale * 20f);
+    }
+}
