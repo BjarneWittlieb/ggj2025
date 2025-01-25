88c7413e53214304f7dd7acac8cea481ba86458b
muhsfeldt <carsten.muhsfeldt@adesso.de>
2025-01-25 21:06:41 +0100

    stars appear and disappear based on score

--- Assets/ScoreLogic.cs
+++ Assets/ScoreLogic.cs
@@ -1,11 +1,17 @@
 using TMPro;
-using Unity.Burst.Intrinsics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreLogic : MonoBehaviour
 {
     private int _currentScore = 0;
     public TMP_Text scoreText;
+    public int scoreForStar1;
+    public int scoreForStar2;
+    public int scoreForStar3;
+    public Image star1;
+    public Image star2;
+    public Image star3;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +22,21 @@ public class ScoreLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_currentScore >= scoreForStar3 && !star3.enabled)
+        {
+            star1.enabled = true; 
+            star2.enabled = true; 
+            star3.enabled = true; 
+        }
+        else if (_currentScore >= scoreForStar2 && !star2.enabled)
+        {
+            star2.enabled = true; 
+            star1.enabled = true; 
+        }        
+        else if (_currentScore >= scoreForStar1 && !star1.enabled)
+        {
+            star1.enabled = true; 
+        }
     }
 
     public void AddToScore(int scoreToAdd)
