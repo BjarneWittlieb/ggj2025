0718d9760fc1fd572c09f8e7a37eb7458378a81e
Bjarne Wittlieb <bjarne.wittlieb@adesso.de>
2025-01-25 15:55:41 +0100

    Bubbles are now changed on select

--- Assets/Scripts/Selection/BubblePlacer.cs
+++ Assets/Scripts/Selection/BubblePlacer.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Data;
+using DefaultNamespace;
+using Models;
+using UnityEngine;
 
 namespace Selection
 {
@@ -9,6 +13,30 @@ namespace Selection
         void Start()
         {
             _bubbleSelector = GetComponent<BubbleSelector>();
+
+            _bubbleSelector.OnSelect += HandleSelect;
+        }
+
+        void OnDestroy()
+        {
+            _bubbleSelector.OnSelect -= HandleSelect;
+        }
+
+        void HandleSelect(GameObject bubble, BubbleType selectedType)
+        {
+            // The bubble is now the new bubble type, So delete previous bubble type
+            BubbleBase bubbleBase = bubble.GetComponent<BubbleBase>();
+            Destroy(bubbleBase);
+            
+            switch (selectedType)
+            {
+                case AreaBubbleType areaBubbleType:
+                    var areaBubble = gameObject.AddComponent<AreaBubble>();
+                    areaBubble.bubbleType = areaBubbleType;
+                    break;
+                default:
+                    throw new NotImplementedException("Currently only area bubble is placeable.");
+            }
         }
     }
 }
