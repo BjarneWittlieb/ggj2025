8d1a62d6059aa67c3c1c804d827955fdda33b5c7
muhsfeldt <carsten.muhsfeldt@adesso.de>
2025-01-25 19:54:25 +0100

    0+1=1

--- Assets/BubbleBase.cs
+++ Assets/BubbleBase.cs
@@ -7,18 +7,23 @@ using Utils;
 public class BubbleBase : MonoBehaviour
 {
     private bool _isPopped = false;
+    private int _value = 1;
+    private ScoreLogic scoreLogic;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected virtual void Start()
     {
+        scoreLogic = GameObject.Find("ScoreLogic").GetComponent<ScoreLogic>();
     }
-
+    
     public virtual void Pop()
     {
         if (!_isPopped)
         {
             _isPopped = true;
         }
+        
+        scoreLogic.AddToScore(_value);
         
         // TODO start pop animation and sound and such
         Destroy(gameObject);
--- Assets/Bubbleplacer.cs
+++ Assets/Bubbleplacer.cs
@@ -10,18 +10,9 @@ public class Bubbleplacer : MonoBehaviour
 
     public Vector2Int CurrentPosition { get; private set; }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        tilemap = grid.GetComponentInChildren<Tilemap>();
-    }
-
     public void placeOnGridPosition(int x, int y)
     {
-        if (tilemap == null)
-        {
-            tilemap = grid.GetComponentInChildren<Tilemap>();
-        }
+        tilemap = grid.GetComponentInChildren<Tilemap>();
         transform.position = tilemap.CellToWorld(new Vector3Int(x, y, 0));
         
         CurrentPosition = new Vector2Int(x, y);
