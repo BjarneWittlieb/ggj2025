92d58d1b3bcf9b683d9cdffa015c87e12ef48d98
muhsfeldt <carsten.muhsfeldt@adesso.de>
2025-01-26 15:34:08 +0100

    random level shit

---
+++ Assets/Scripts/Utils/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Utils
+{
+    public static class ListExtensions
+    {
+        public static List<T> AddMultiple<T>(this List<T> list, T item, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(item);
+            }
+
+            return list;
+        }
+    }
+}
